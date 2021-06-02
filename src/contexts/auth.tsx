6c0bb0a08983f8fc10a2e5ea8e4dff3340e3d03c
Douglas Pereira <62361358+douglasspereira@users.noreply.github.com>
2021-06-02 16:26:22 -0300

    rotas

---
+++ src/contexts/auth.tsx
@@ -0,0 +1,27 @@
+import React, {createContext, useState} from 'react';
+import * as auth from '../services/auth';
+
+interface AuthContextData {
+  signed: boolean;
+  user: object | null;
+  signIn(): Promise<void>;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+export const AuthProvider = ({children}) => {
+  const [user, setUser] = useState<object | null>(null);
+
+  async function signIn() {
+    const response = await auth.signIn();
+
+    setUser(response.user)
+  }
+  return (
+    <AuthContext.provider value={{signed: !!user, user, signIn: signIn}}>
+      {children}
+    </AuthContext.provider>
+  );
+};
+
+export default AuthContext;
