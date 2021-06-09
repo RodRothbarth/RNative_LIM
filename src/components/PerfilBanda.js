2bd4fac721a3af827a2323b7e4c9e8f60d0deadd
Luciano <luciano_alexandre-j1@estudante.sc.senai.br>
2021-06-08 22:10:56 -0300

    08/06/2021

--- src/components/PerfilBanda.js
+++ src/components/PerfilBanda.js
@@ -7,63 +7,87 @@ import {
   Text,
   ImageBackground
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const PerfilBanda = ({navigation}) => {
   return(
-    <ImageBackground source={require('../assets/stage-lights.jpg')} style={styles.image}>
-    <View style={styles.container}>
+    // <ImageBackground source={require('../assets/stage-lights.jpg')} style={styles.image}>
+    <View style={styles.pageNavegacao}>
+      <Text style={styles.text1}>Navegação</Text>
 
-      <Text style={styles.text}>Os Melhores Eventos</Text>
-      <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('ListaEventosBanda')}>
-        <Text style={styles.text}>Eventos</Text>
-      </TouchableOpacity>
+       
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('ListaEstabelecimentos')}>
+          <Icon style={styles.icon} name="home" size={30}></Icon> 
+          <View style={styles.aroundText}>
+            <Text style={styles.text}>Estabelecimentos</Text>
+            <Text style={styles.small}>Lugares para eventos</Text>
+          </View>
+          <Icon style={styles.icon} name="chevron-right" size={15}></Icon> 
+        </TouchableOpacity>
 
-      <Text style={styles.text}>Os Melhores Lugares</Text>
-      <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('ListaEstabelecimentos')}>
-        <Text style={styles.text}>Estabelecimentos</Text>
-      </TouchableOpacity>
-      
+        <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('ListaEventosBanda')}>
+          <Icon style={styles.icon} name="calendar-check" size={35}></Icon> 
+          <View style={styles.aroundText}>
+            <Text style={styles.text}>Eventos</Text>
+            <Text style={styles.small}>Eventos disponíveis</Text>
+          </View>
+          <Icon style={styles.icon} name="chevron-right" size={15}></Icon> 
+        </TouchableOpacity>
+      </View>
     </View>
-    </ImageBackground>
+    // </ImageBackground>
   )
 }
 const styles = StyleSheet.create({
-  container: {
-    
+  pageNavegacao: {
     flex: 1,
-    borderWidth: 1,
-    alignContent: 'center',
-    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#2a2a2a'
   },
-text:{
-  color: '#fff',
-  fontWeight: 'bold',
-  fontSize: 20
-},
-
-text1:{
-  color: '#fff',
-  fontWeight: 'bold',
-  fontSize: 25,
-  textAlign: 'center'
-},
-
-image:{
-  width:'100%',
-  height:'100%'
-},
-
+  container: {
+    alignItems: 'center',
+    width: '100%'
+  },
+  text1:{
+    color: '#fff',
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 30,
+    fontSize: 25,
+    textAlign: 'center'
+  },
+  image:{
+    width:'100%',
+    height:'100%'
+  },
   button: {
-    width: '70%',
-    marginVertical: 20,
-    height: 50,
-    backgroundColor: '#00cc9980',
+    width: '100%',
+    flexDirection: 'row',
+    marginVertical: 10,
+    height: 70,
+    borderBottomWidth: 1,
+    borderBottomColor: '#fff',
     padding: 5,
     borderRadius: 25,
-    alignContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: 20
+  },
+  icon: {
+    marginRight: 30,
+    marginLeft: 20,
+    color: '#fff',
+  },
+  aroundText : {
+    width: 200
+  },
+  text:{
+    color: '#fff',
+    fontSize: 20,
+  },
+  small:{
+    color: '#fff',
   },
 })
 
--- src/components/PerfilLocal.js
+++ src/components/PerfilLocal.js
@@ -8,67 +8,113 @@ import {
   ImageBackground
 } from 'react-native';
 
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
 const PerfilBanda = ({navigation}) => {
   return(
-    <ImageBackground source={require('../assets/stage-lights.jpg')} style={styles.image}>
-    <View style={styles.container}>
+    <View style={styles.pageNavegacao}>
+      <Text style={styles.text1}>Navegação</Text>
 
-      {/* <Text style={styles.text}>Os Melhores Eventos</Text> */}
-      <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('AddEvento')}>
-        <Text style={styles.text}>Adicionar Evento</Text>
-      </TouchableOpacity>
+       
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('AddEvento')}>
+          <Icon style={styles.icon} name="plus" size={30}></Icon> 
+          <View style={styles.aroundText}>
+            <Text style={styles.text}>Adicionar Evento</Text>
+            <Text style={styles.small}>Crie um evento</Text>
+          </View>
+          <Icon style={styles.icon} name="chevron-right" size={15}></Icon> 
+        </TouchableOpacity>
 
-      {/* <Text style={styles.text}>Os Melhores Eventos</Text> */}
-      <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('ListaEventosLocal')}>
-        <Text style={styles.text}>Meus Eventos</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('ListaEventosLocal')}>
+          <Icon style={styles.icon} name="paste" size={35}></Icon> 
+          <View style={styles.aroundText}>
+            <Text style={styles.text}>Meus Eventos</Text>
+            <Text style={styles.small}>Eventos que você criou</Text>
+          </View>
+          <Icon style={styles.icon} name="chevron-right" size={15}></Icon> 
+        </TouchableOpacity>
 
-      {/* <Text style={styles.text}>Os Melhores Artistas</Text> */}
-      <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('ListaBandas')}>
-        <Text style={styles.text}>Bandas</Text>
-      </TouchableOpacity>
-      
+        <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('ListaBandas')}>
+          <Icon style={styles.icon} name="guitar" size={35}></Icon> 
+          <View style={styles.aroundText}>
+            <Text style={styles.text}>Bandas</Text>
+            <Text style={styles.small}>Bandas disponíveis</Text>
+          </View>
+          <Icon style={styles.icon} name="chevron-right" size={15}></Icon> 
+        </TouchableOpacity>
+      </View>
     </View>
-    </ImageBackground>
+    // <View style={styles.container}>
+
+    //   {/* <Text style={styles.text}>Os Melhores Eventos</Text> */}
+    //   <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('AddEvento')}>
+    //     <Text style={styles.text}>Adicionar Evento</Text>
+    //   </TouchableOpacity>
+
+    //   {/* <Text style={styles.text}>Os Melhores Eventos</Text> */}
+    //   <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('ListaEventosLocal')}>
+    //     <Text style={styles.text}>Meus Eventos</Text>
+    //   </TouchableOpacity>
+
+    //   {/* <Text style={styles.text}>Os Melhores Artistas</Text> */}
+    //   <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate('ListaBandas')}>
+    //     <Text style={styles.text}>Bandas</Text>
+    //   </TouchableOpacity>
+      
+    // </View>
+    // </ImageBackground>
   )
 }
 const styles = StyleSheet.create({
-  container: {
-    
+  pageNavegacao: {
     flex: 1,
-    borderWidth: 1,
-    alignContent: 'center',
-    justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#2a2a2a'
   },
-text:{
-  color: '#fff',
-  fontWeight: 'bold',
-  fontSize: 20
-},
-
-text1:{
-  color: '#fff',
-  fontWeight: 'bold',
-  fontSize: 25,
-  textAlign: 'center'
-},
-
-image:{
-  width:'100%',
-  height:'100%'
-},
-
+  container: {
+    alignItems: 'center',
+    width: '100%'
+  },
+  text1:{
+    color: '#fff',
+    fontWeight: 'bold',
+    marginTop: 15,
+    marginBottom: 30,
+    fontSize: 25,
+    textAlign: 'center'
+  },
+  image:{
+    width:'100%',
+    height:'100%'
+  },
   button: {
-    width: '70%',
-    marginVertical: 20,
-    height: 50,
-    backgroundColor: '#00cc9980',
+    width: '100%',
+    flexDirection: 'row',
+    marginVertical: 10,
+    height: 70,
+    borderBottomWidth: 1,
+    borderBottomColor: '#fff',
     padding: 5,
     borderRadius: 25,
-    alignContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    paddingLeft: 20
+  },
+  icon: {
+    marginRight: 30,
+    marginLeft: 20,
+    color: '#fff',
+  },
+  aroundText : {
+    width: 200
+  },
+  text:{
+    color: '#fff',
+    fontSize: 20,
+  },
+  small:{
+    color: '#fff',
   },
 })
 
